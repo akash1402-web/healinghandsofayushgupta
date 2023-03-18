6754fe758a6f5ad2ae2ec95114be048c1d3529e7
akash_singh <66346350+akash1402-web@users.noreply.github.com>
2023-03-18 23:26:33 +0530

    Update Testimonial.jsx

--- src/components/Home/Testimonial.jsx
+++ src/components/Home/Testimonial.jsx
@@ -21,7 +21,7 @@ const Testimonial = () => {
         <div id="client">
 
             <h1 className='text-center font-[600px] text-2xl  mt-5 lg:hidden '>what our client has to say</h1>
-            <div className='w-full flex gap-10 mt-[70px] justify-between '>
+            <div className='w-full lg:flex gap-10 mt-[70px] justify-between  hidden'>
                 <div className='lg:flex flex-col gap-5 hidden'>
                     <h3 className='text-4xl w-[280px] '>What our client has to say</h3>
                     <div className='flex gap-3 mt-[78px] items-center'>
@@ -32,12 +32,16 @@ const Testimonial = () => {
                     <a href="#"><button className="px-[42px] py-[13px] bg-[#8777D7] w-max rounded-lg text-white font-bold">Read All</button></a>
                 </div>
 
-                <Swiper spaceBetween={20} slidesPerView={2} grabCursor={2} className='mySwiper'
+                <Swiper spaceBetween={20} slidesPerView={2} grabCursor={2} className='mySwiper '
                     navigation={{
                         prevEl: ".swiper-button-prev",
                         nextEl: ".swiper-button-next",
                     }}
                     loop={true}
+                    pagination={{
+                        clickable: true,
+                        el: ".achivement-dots",
+                    }}
                     modules={[
                         Pagination
                     ]
@@ -61,8 +65,49 @@ const Testimonial = () => {
                     <SwiperSlide>
                         <TestimonialCard />
                     </SwiperSlide>
+
                 </Swiper>
+
             </div>
+            <div className="lg:hidden block">
+                <Swiper spaceBetween={20} slidesPerView={2} grabCursor={2} className='mySwiper mb-4'
+                    navigation={{
+                        prevEl: ".swiper-button-prev",
+                        nextEl: ".swiper-button-next",
+                    }}
+                    loop={true}
+                    pagination={{
+                        clickable: true,
+                        el: ".achivement-dots",
+                    }}
+                    modules={[
+                        Pagination
+                    ]
+                    }
+                    onInit={(swiper) => {
+                        swiper.params.navigation.prevEl = prevRef.current;
+                        swiper.params.navigation.nextEl = nextRef.current;
+                        swiper.navigation.init();
+                        swiper.navigation.update();
+                    }}>
+
+                    <SwiperSlide>
+                        <TestimonialCard />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <TestimonialCard />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <TestimonialCard />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <TestimonialCard />
+                    </SwiperSlide>
+
+                </Swiper>
+                <div className='achivement-dots text-center'></div>
+            </div>
+
             <a><button className='w-[100%] h-[40px] rounded-md  bg-white  border-2 border-[#282828] mt-8  md:hidden hover:bg-navitem'>View All</button></a>
         </div>
     )
