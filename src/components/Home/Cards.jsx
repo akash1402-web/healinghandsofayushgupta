d431334b5f34f2e6cdcf7760d5a650a1281d6252
akash_singh <66346350+akash1402-web@users.noreply.github.com>
2023-03-18 23:24:06 +0530

    Update Cards.jsx

--- src/components/Home/Cards.jsx
+++ src/components/Home/Cards.jsx
@@ -18,7 +18,7 @@ const Cards = () => {
                 <div className='flex flex-col gap-3 md:w-[200px] w-[115px] items-center px-2'>
                     <img src={health} alt="health" className='md:w-[82px] md:h-[82px] w-[52px] h-[52px] ' />
                     <h3 className='font-bold'>Health</h3>
-                    <p className='md:text-sm text-xs text-center'>Achieve physical, mental, emotional, and spiritual well-being</p>
+                    <p className='md:text-sm text-xs text-center'>Become physically, mentally, emotionally and spiritually well-being.</p>
                 </div>
                 <div className='md:w-[2px] w-[2px] md:h-[200px] h-[180px] bg-[#E9E9E9] '>
 
@@ -26,28 +26,28 @@ const Cards = () => {
                 <div className='flex flex-col gap-3 md:w-[200px] w-[115px] items-center  px-2'>
                     <img src={abundance} alt="health" className='md:w-[82px] md:h-[82px] w-[52px] h-[52px]' />
                     <h3 className='font-bold'>Abundance</h3>
-                    <p className='md:text-sm text-xs text-center'>Achieve physical, mental, emotional, and spiritual well-being</p>
+                    <p className='md:text-sm text-xs text-center'>Attract happiness, success and great moments and money.</p>
                 </div>
                 <div className='md:w-[2px] w-[2px] md:h-[200px] h-[180px] bg-[#E9E9E9] ' />
 
                 <div className='flex flex-col gap-3 md:w-[200px] w-[115px] items-center  px-2'>
                     <img src={relationship} alt="health" className='md:w-[82px] md:h-[82px] w-[52px] h-[52px]' />
                     <h3 className='font-bold'>Relationships</h3>
-                    <p className='md:text-sm text-xs text-center'>Achieve physical, mental, emotional, and spiritual well-being</p>
+                    <p className='md:text-sm text-xs text-center'>Strengthening strained connections will improve communications and passions.</p>
                 </div>
                 <div className='md:w-[2px]  md:h-[200px] h-[180px] md:block hidden bg-[#E9E9E9] ' />
 
                 <div className='flex flex-col gap-3 md:w-[200px] w-[115px] items-center  px-2'>
                     <img src={peace} alt="health" className='md:w-[82px] md:h-[82px] w-[52px] h-[52px]' />
                     <h3 className='font-bold'>Peace</h3>
-                    <p className='md:text-sm text-xs text-center'>Achieve physical, mental, emotional, and spiritual well-being</p>
+                    <p className='md:text-sm text-xs text-center'>Discover enduring happiness, tranquillity and liveliness.</p>
                 </div>
                 <div className='md:w-[2px] w-[2px] md:h-[200px] h-[180px]  bg-[#E9E9E9]  ' />
 
                 <div className='flex flex-col gap-3 md:w-[200px] w-[115px] items-center  px-2'>
                     <img src={growth} alt="health" className='md:w-[82px] md:h-[82px] w-[52px] h-[52px]' />
                     <h3 className='font-bold text-center'>Personal Growth</h3>
-                    <p className='md:text-sm text-xs text-center'>Achieve physical, mental, emotional, and spiritual </p>
+                    <p className='md:text-sm text-xs text-center'>Discover your life’s purpose, boost your confidence and enhance your intuition.</p>
                 </div>
             </div>
         </>
